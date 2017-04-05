3ae3021dbf55cb1836951eacc2872f316d2355aa
carryme9527 <carryme9527@users.noreply.github.com>
2017-04-06 03:02:43 +0800

    fix typo

--- pystocktw/crawl/util.py
+++ pystocktw/crawl/util.py
@@ -7,7 +7,7 @@ def get_headers():
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, sdch, br',
         'Accept-Language': 'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-        'Connection': 'keep-alivea',
+        'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
         'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) '
                        'AppleWebKit/537.36 (KHTML, like Gecko) '
